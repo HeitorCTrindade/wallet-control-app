8367891279e5b94cab2923551a20b21998a3045f
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-08-26 19:34:31 -0300

    Requisito 2 - finalizado.

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,30 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <section>
+        <div>Bem vindo ao Trybe Wallet!</div>
+        <input
+          type="email"
+          name="email"
+          value={ email }
+          data-testid="email-input"
+          onChange={ this.handleChanges }
+        />
+        <input
+          type="password"
+          name="senha"
+          value={ senha }
+          data-testid="password-input"
+          onChange={ this.handleChanges }
+        />
+        <button
+          type="button"
+          disabled={ isLoginButtonDisabled }
+          onClick={ this.handleLoginClickButton }
+        >
+          Entrar
+        </button>
+      </section>
     );
   }
 }
