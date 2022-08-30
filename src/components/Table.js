4785bf51cb2598a21b5245ed14273bf685b5c3c5
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-08-30 18:34:45 -0300

    Requisito 6- finalizado.

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table className="th">
+          <thead>
+            <tr>
+              <th className="th-0lax">Descrição</th>
+              <th className="th-0lax">Tag</th>
+              <th className="th-0lax">Método de pagamento</th>
+              <th className="th-0lax">Valor</th>
+              <th className="th-0lax">Moeda</th>
+              <th className="th-0lax">Câmbio utilizado</th>
+              <th className="th-0lax">Valor convertido</th>
+              <th className="th-0lax">Moeda de conversão</th>
+              <th className="th-0lax">Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
